d8e042f605e449823cb75a96c1241695d9a25351
Anna Povey <annarobot4@gmail.com>
2022-07-27 14:41:40 -0700

    testing file

--- testing.py
+++ testing.py
@@ -1,2 +1,7 @@
-f2 = open("abbreviations.txt", "r")
-print(f2.read().split())
+from rand_spl.search_abbreviations import generate_abbreviations
+from rand_spl.__init__ import generate_txt
+
+print("here")
+generate_abbreviations(1000)
+print("here")
+generate_txt(1000, False)
